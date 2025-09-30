00b19bda87dd1ef7ad9f2b6065ef545e2c0349a0
Ryocery <youri.haentjens@gmail.com>
2025-09-30 11:34:43 +0200

    Added basic functionality for word validation

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use std::collections::HashSet;
+
+pub struct WordValidator {
+    words: HashSet<String>,
+}
+
+impl WordValidator {
+    pub fn new() -> Self {
+        let words_extract = include_str!("../data/words.txt");
+        let words: HashSet<String> = words_extract
+            .lines()
+            .map(|s| s.trim().to_lowercase())
+            .collect();
+        Self { words }
+    }
+
+    pub fn is_valid(&self, word: &str) -> bool {
+        self.words.contains(&word.to_lowercase())
+    }
+}
